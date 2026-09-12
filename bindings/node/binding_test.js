d8377be316bdf26dc3788a6024f9e3c3a1b751a2
melon <132408878+meloncholic@users.noreply.github.com>
2026-09-11 22:09:28 -0400

    fix(node): correct binding package contract (#16)

--- bindings/node/binding_test.js
+++ bindings/node/binding_test.js
@@ -2,10 +2,10 @@ import assert from "node:assert";
 import { test } from "node:test";
 import Parser from "tree-sitter";
 
-test("can load grammar", () => {
+test("can load grammar", async () => {
   const parser = new Parser();
-  assert.doesNotReject(async () => {
-    const { default: language } = await import("./index.js");
+  const { default: language } = await import("./index.js");
+  assert.doesNotThrow(() => {
     parser.setLanguage(language);
   });
 });
